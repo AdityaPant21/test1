60f0e112880015f198933c20b090ce288b4d2590
sdac-17 <sdac-17@sdac-17-PC>
2024-06-26 14:39:11 -0700

    Update1

--- Test/src/Main2.java
+++ Test/src/Main2.java
@@ -1,19 +1,20 @@
 class A{
-	void meth(){
-		System.out.println("Aditya");
+	void meth(int a){
+		System.out.println(a);
 	}
 }
 public class Main2 extends A {
 	
 	@Override
-	void meth() {
+	void meth(int a) {
 		// TODO Auto-generated method stub
-		System.out.println("Kaif");
+		System.out.println(a);
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Main2 m = new Main2();
+		m.meth(10);
 	}
 
 }
