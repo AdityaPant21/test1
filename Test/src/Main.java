36bdefe3894723a9df3c26be80d40aa6db6f3149
sdac-17 <sdac-17@sdac-17-PC>
2024-06-26 14:27:27 -0700

    Test1

---
+++ Test/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	void a(){
+		System.out.println("Aditya");
+	}
+	
+	void a(int a1){
+		System.out.println(a1);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main name = new Main();
+		name.a();
+		name.a(10);
+	}
+
+}
---
+++ Test/src/Main2.java
@@ -0,0 +1,13 @@
+
+public class Main2 {
+	
+	void meth(){
+		System.out.println("Aditya");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
