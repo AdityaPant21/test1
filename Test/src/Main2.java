65d5de6b19132e79400c6bde919d35081806a52e
sdac-17 <sdac-17@sdac-17-PC>
2024-06-26 14:33:56 -0700

    Update

--- Test/src/Main2.java
+++ Test/src/Main2.java
@@ -1,8 +1,14 @@
-
-public class Main2 {
-	
+class A{
 	void meth(){
 		System.out.println("Aditya");
+	}
+}
+public class Main2 extends A {
+	
+	@Override
+	void meth() {
+		// TODO Auto-generated method stub
+		System.out.println("Kaif");
 	}
 	
 	public static void main(String[] args) {
